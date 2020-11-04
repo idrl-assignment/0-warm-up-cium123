598ac18905a6c4c23469f79fb0452d50862904c9
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-04 09:30:54 +0800

    Update plot_matrix.py

--- plot_matrix.py
+++ plot_matrix.py
@@ -4,11 +4,9 @@ import scipy
 
 
 def generate_random_matrix(m, n):
-    #raise NotImplementedError  # TODO: 删除该行，实现该函数功能
     return(np.random.randint(0,2,(m,n))
 
 def save_matrix(matrix, file_name):
-    #raise NotImplementedError  # TODO: 删除该行，实现该函数功能
     return(scipy.misc.imsave(file_name,matrix))
 
 if __name__ == "__main__":
