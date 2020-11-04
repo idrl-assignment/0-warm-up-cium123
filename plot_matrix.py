b639dfba0289d24d328267abc07dfe7e5b0d48b9
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-04 09:53:51 +0800

    Update plot_matrix.py

--- plot_matrix.py
+++ plot_matrix.py
@@ -1,13 +1,10 @@
 # TODO: 
 import numpy as np
 import imageio
-
 def generate_random_matrix(m, n):
     return(np.random.randint(0,2,(m,n)))
-
 def save_matrix(matrix, file_name):
     return(imageio.imwrite(file_name,matrix))
-
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
     save_matrix(matrix, "example.jpg")
